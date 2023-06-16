f318fa6d9c9df8f53e2376785dd75312c9061686
Danilo Miguel <danilo_salvato@hotmail.com>
2023-06-16 11:48:05 -0300

    Atualizando

--- WorkerService/App/Interface/IHttpService.cs
+++ WorkerService/App/Interface/IHttpService.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net;
 
 namespace WorkerService.App.Interface
 {
     public interface IHttpService
     {
-        Task<HttpStatusCode> CheckStatusSite(string url);   
+        Task<HttpStatusCode> CheckStatusSite(string url);
     }
 }
--- WorkerService/App/Service/EmailService.cs
+++ WorkerService/App/Service/EmailService.cs
@@ -1,29 +1,21 @@
-﻿using dotenv.net;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WorkerService.App.Interface;
+﻿using WorkerService.App.Interface;
 using WorkerService.Helper;
 
 namespace WorkerService.App.Service
-{   
+{
     public class EmailService : IEmail
     {
         private readonly IConfiguration _config;
 
         public EmailService(IConfiguration config)
         {
-
             _config = config ?? throw new ArgumentNullException(nameof(config));
-
         }
 
         public void SendEmail(string to, string subject, string body)
         {
             string password = _config.GetSection("settings").GetSection("Password").Value;
-            var outlook = new EmailHelper("smtp.office365.com", "dannmiguel95@hotmail.com", password);
+            var outlook = new EmailHelper("smtp.office365.com", "Teste.envia@hotmail.com", password);
 
             outlook.SendEmail(new List<string> { to }, subject, body);
         }
